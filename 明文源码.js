2e0486908ad61f4ae228d765476a3fce71f3e9a9
johndeanh <100190445+johndeanh@users.noreply.github.com>
2025-03-15 23:28:46 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from "cloudflare:sockets";
 
-let password = '';
+let password = '2c1bcc9d-0bfb-42fe-bbbe-8d5663296cc6';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
